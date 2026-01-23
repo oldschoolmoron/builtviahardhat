c4b67fbe9e38740382c159e8c7a4dd1a77b2724d
Ananya Pathak <anyalko043@gmail.com>
2026-01-23 22:53:40 +0530

    chore: wire MyToken

--- contracts/TimeInvestment.sol
+++ contracts/TimeInvestment.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import './MyToken.sol';
+
 // interface IERC20 {
 //     function transferFrom(address from, address to, uint256 amount) external returns (bool);
 //     function transfer(address to, uint256 amount) external returns (bool);
