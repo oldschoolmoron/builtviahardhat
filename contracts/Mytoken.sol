c3d7fa7387f4f9902c947ddbc603325ab0e2eda2
Ananya Pathak <anyalko043@gmail.com>
2026-01-23 22:37:04 +0530

    feat: initial ERC20 token implementation

---
+++ contracts/Mytoken.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+    constructor() ERC20("My Token", "MTK") {
+        _mint(msg.sender, 10_000 ether);
+    }
+}
